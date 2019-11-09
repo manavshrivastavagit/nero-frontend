94ae76fbf84bc388c980fea89f4655dd31248c19
manavshrivastava <manavshrivastava@hotmail.com>
2019-11-09 16:43:01 +0530

    updated

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-var Message;
+var Message, MessageLoading;
 $messages = $('.messages');
 Message = function (arg) {
 		this.text = arg.text, this.message_side = arg.message_side, this.time = arg.time;
@@ -16,6 +16,24 @@ Message = function (arg) {
 		}(this);
 		return this;
 };
+
+MessageLoading = function (arg) {
+		this.text = arg.text, this.message_side = arg.message_side, this.time = arg.time;
+		this.draw = function (_this) {
+				return function () {
+						var $message;
+						$message = $($('.loading_message_template').clone().html());
+						$message.addClass(_this.message_side).find('.text_loading').html('<div class="avatar avatar_loading"></div>');
+						$message.addClass('loading_appeared');
+						$('.messages').append($message);
+						return setTimeout(function () {
+								return $message.addClass('appeared');
+						}, 0);
+				};
+		}(this);
+		return this;
+};
+
 function addBr(text){
 		//console.log("text was: "+text)
 		newText=text.replace(/\n/g, "<br />")
@@ -58,4 +76,38 @@ function showBotMessage(msg,d){
 				});
 				message.draw();
 				$messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+}
+
+function showWelcomeMessage(){
+	 $('.message_input').val('');
+         $('.message_input').focus();
+         
+         var welcomeMsg = "";
+         if(!localStorage.getItem("firstname")){
+             welcomeMsg = "Hi! This is NERO, your personal digital assistant for anything and everything related to Enquero"
+              showBotMessage(welcomeMsg,getCurrentTimestamp());
+          AskFNameLNameMsg = "Please provide your First-name and Last-name\n example: Ajit Verma"
+          showBotMessage(AskFNameLNameMsg,getCurrentTimestamp());
+         } else {
+            welcomeMsg = "Hi " + localStorage.getItem("firstname")+ " " + localStorage.getItem("lastname") + " ! My name is NERO and I'm a Chatbot. I can help you with Enquero related queries. Please select a quick link below or type your question in the space provided."
+          showBotMessage(welcomeMsg,getCurrentTimestamp());
+         }
+}
+
+function showLoading(){
+	var options = {month: 'short', day: 'numeric', hour:'numeric', minute: 'numeric'  };
+				console.log("in showLoadingMessage");
+				MessageLoading = new MessageLoading({
+						text: 'loading...',
+						time: getCurrentTimestamp().toLocaleString("en-IN", options),
+						message_side: 'left'
+				});
+				MessageLoading.draw();
+				$messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+}
+
+function hideLoading(){
+	//$('.messages').remove();
+	$( "li.message.left.appeared.loading_appeared" ).hide();
+	// $('.messages').remove('loading_appeared')
 }
